dc868c1eda0d39f833ea5e9d4f0669bdbd1df4de
KachanAnna <82448186+KachanAnna@users.noreply.github.com>
2023-02-04 17:08:54 +0700

    commit

--- World/booksweb/booksweb/urls.py
+++ World/booksweb/booksweb/urls.py
@@ -1,18 +1,19 @@
 from django.conf import settings
 from django.urls import path, re_path
 from django.views.static import serve
-
-from .views import main_page
+from .views import main_page, authors_add, authors_create, authors_delete,  authors_edit
 from .views import BookListView, BookDetailView, AuthorListView
-
 
 urlpatterns = [
     path('', main_page, name='main_page'),
+    path('authors_add/', authors_add, name='authors_add'),
+    path('authors_edit/<int:id>', authors_edit, name='authors_edit'),
+    path('authors_create/', authors_create, name='authors_create'),
+    path('authors_delete/<int:id>', authors_delete, name='authors_delete'),
     re_path(r'^books/$', BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', BookDetailView.as_view(), name='book_detail'),
     re_path(r'^authors/$', AuthorListView.as_view(), name='authors'),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-
 
 ]
 
--- World/booksweb/booksweb/views.py
+++ World/booksweb/booksweb/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from .models import Book, Author
 from django.shortcuts import render
+from .forms import AuthorsForm
+from django.http.response import HttpResponseRedirect, HttpResponseNotFound
 
 
 def main_page(request):
@@ -18,6 +20,60 @@ def main_page(request):
             'num_visits': num_visits
         },
     )
+
+
+def authors_add(request):
+    author = Author.objects.all()
+    authorsform = AuthorsForm()
+    return render(
+        request,
+        'booksweb/authors_add.html',
+        context={
+            'form': authorsform,
+            'author': author
+        },
+    )
+
+
+def authors_create(request):
+    if request.method == 'POST':
+        form = AuthorsForm(request.POST)
+        if form.is_valid():
+            author = Author()
+            author.first_name = request.POST.get('first_name')
+            author.last_name = request.POST.get('last_name')
+            author.date_of_birth = request.POST.get('date_of_birth')
+            author.date_of_death = request.POST.get('date_of_death ')
+            author.save()
+            return HttpResponseRedirect('/authors_add/')
+
+
+def authors_delete(request, id):
+    try:
+        author = Author.objects.get(id=id)
+        author.delete()
+        return HttpResponseRedirect('/authors_add/')
+    except Author.DoesNotExist:
+        return HttpResponseNotFound("<h2>Автор не найден</h2>")
+
+
+def authors_edit(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == 'POST':
+        author.first_name = request.POST.get('first_name')
+        author.last_name = request.POST.get('last_name')
+        author.date_of_birth = request.POST.get('date_of_birth')
+        author.date_of_death = request.POST.get('date_of_death ')
+        author.save()
+        return HttpResponseRedirect('/authors_add/')
+    else:
+        return render(
+            request,
+            'booksweb/authors_edit.html',
+            context={
+                'author': author
+            }
+        )
 
 
 class BookListView(generic.ListView):
